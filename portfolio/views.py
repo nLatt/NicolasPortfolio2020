62b0f493a037acd92114ff6aca85556f907db4e5
Nicolas Lattemann <56591727+Nydragon00@users.noreply.github.com>
2020-06-16 17:35:55 +0200

    os indepedent path

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,11 +3,10 @@ from django.template import Context, loader
 import os
 
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'relative/path/to/file/you/want')
 
 # Create your views here.
 
 def header(request):
-    filename = os.path.join(dirname, 'templates\\portfolio\\hello_world.html')
-    template = loader.get_template(filename)#"portfolio\\hello_world.html")
+    filename = os.path.join("portfolio", "hello_world.html")
+    template = loader.get_template(filename)
     return HttpResponse(template.render())
