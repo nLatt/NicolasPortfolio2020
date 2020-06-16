c65490ed00b5c1ee6786c45c9f277b94264b8492
Nicolas Lattemann <56591727+Nydragon00@users.noreply.github.com>
2020-06-16 12:12:23 +0200

    update

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,6 +8,6 @@ filename = os.path.join(dirname, 'relative/path/to/file/you/want')
 # Create your views here.
 
 def header(request):
-    filename = os.path.join(dirname, 'templates\\portfolio\\hello_world.html')
+    filename = os.path.join(dirname, 'template/portfolio/hello_world.html')
     template = loader.get_template(filename)
     return HttpResponse(template.render())
