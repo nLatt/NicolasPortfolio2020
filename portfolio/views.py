209b4b2ca166dfd63f6e659d0385e6a280bce638
Nicolas Lattemann <56591727+Nydragon00@users.noreply.github.com>
2020-06-18 16:53:41 +0200

    github cards now generated through a for loop

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import loader
 import os
 
 # Create your views here.
+repo_list = ["date-a-scientist", "Game_Of_Life", "machine_learning_codeacademy", "Game_Of_Life", "machine_learning_codeacademy", "Game_Of_Life", "machine_learning_codeacademy"]
+
+variables = {
+    "repos": repo_list,
+    "user": "nLatt"
+}
 
 def header(request):
     filename = os.path.join("portfolio", "main.html")
     template = loader.get_template(filename)
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(variables))
